845b16f6c21643cbcf9d269dbbd9a1ead6531bb6
Jesus Arturo Carmona Aguirre <jessarturo97@gmail.com>
2023-01-07 00:24:40 -0600

    Agregue traducciones con i18next

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { HiLanguage } from "react-icons/hi2";
+import ArrowDown from "../assets/arrow-down.svg";
+import MxFlag from "../assets/mx-flag.svg";
+import UsFlag from "../assets/us-flag.svg";
 import "../styles/header.scss";
 
 function Header() {
+  const [t, i18n] = useTranslation("global");
+
+  const [isOpen, setIsOpen] = useState(false);
+  const btnRef = useRef();
+
+  useEffect(() => {
+    const closeDropdown = (e) => {
+      if (e.path[0] !== btnRef.current) {
+        setIsOpen(false);
+      }
+    };
+
+    document.body.addEventListener("click", closeDropdown);
+
+    return () => document.body.removeEventListener("click", closeDropdown);
+  }, []);
+
   return (
     <header className="header container">
       <a href="#" className="logo">
@@ -11,35 +34,58 @@ function Header() {
         <ol className="nav_list">
           <li className="nav_item">
             <a className="nav_link" href="#">
-              Sobre mí
+              {t("header.about")}
             </a>
           </li>
           <li className="nav_item">
             <a className="nav_link" href="#">
-              Experiencia
+              {t("header.experience")}
             </a>
           </li>
           <li className="nav_item">
             <a className="nav_link" href="#">
-              Proyectos
+              {t("header.projects")}
             </a>
           </li>
           <li className="nav_item">
             <a className="nav_link" href="#">
-              Contacto
+              {t("header.contact")}
             </a>
           </li>
         </ol>
 
-        <a
+        {/* <a
           className="cv_link"
           href="/public/CV_Jesus_Arturo_CA.pdf"
           download
           target="_blank"
         >
-          Descargar CV
-        </a>
+          {t("header.download")}
+        </a> */}
       </nav>
+
+      <div className="languages">
+        <span className="languages_icon">
+          <HiLanguage />
+        </span>
+        <button
+          ref={btnRef}
+          className="languages_btn"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          {i18n.language}
+          <img className="arrow_icon" src={ArrowDown} alt="" />
+        </button>
+
+        <ul className={`languages_list ${isOpen ? "open" : ""}`}>
+          <li onClick={() => i18n.changeLanguage("en")}>
+            <img src={UsFlag} alt="" /> {t("header.english")}
+          </li>
+          <li onClick={() => i18n.changeLanguage("es")}>
+            <img src={MxFlag} alt="" /> {t("header.spanish")}
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import global_en from "./translations/en/global.json";
+import global_es from "./translations/es/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);
