3f6f28195f16a2b8ff4c9bc4b3a1cab21a210dc7
Jesus Arturo Carmona Aguirre <jessarturo97@gmail.com>
2023-01-08 20:22:31 -0600

    Correcciones en el modo responsive de todas las secciones creadas hasta el momento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { HiLanguage } from "react-icons/hi2";
+import { IoMdClose } from "react-icons/io";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import ArrowDown from "../assets/arrow-down.svg";
 import MxFlag from "../assets/mx-flag.svg";
 import UsFlag from "../assets/us-flag.svg";
+import logo from "../assets/logo.svg";
 import "../styles/header.scss";
 
 export const Header = () => {
   const [t, i18n] = useTranslation("global");
+
+  const [menu, setMenu] = useState(false);
 
   const [isOpen, setIsOpen] = useState(false);
   const btnRef = useRef();
@@ -27,12 +32,12 @@ export const Header = () => {
   return (
     <header className="header_section">
       <nav className="nav_content container">
-        <a href="#" className="logo">
-          <span>{`[`}</span> JS Arturo <span>{`]`}</span>
+        <a href="/">
+          <img src={logo} alt="" className="logo_img" />
         </a>
 
         <div className="nav">
-          <ol className="nav_list">
+          <ol className={`nav_list ${menu ? "open_menu" : ""}`}>
             <li className="nav_item">
               <a className="nav_link" href="#">
                 {t("header.about")}
@@ -54,9 +59,18 @@ export const Header = () => {
               </a>
             </li>
           </ol>
+          <button className="menu_btn" onClick={() => setMenu((prev) => !prev)}>
+            <HiOutlineMenuAlt3 />
+          </button>
+          <button
+            className={`close_menu ${menu ? "open_menu" : ""}`}
+            onClick={() => setMenu(false)}
+          >
+            <IoMdClose />
+          </button>
         </div>
 
-        <div className="languages">
+        <div className={`languages ${menu ? "open_menu" : ""}`}>
           <span className="languages_icon">
             <HiLanguage />
           </span>
